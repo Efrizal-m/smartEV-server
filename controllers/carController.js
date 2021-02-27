73af07b2bfe3e04fbd09663fcb5fdcb2d0ace7e9
efrizal-m <efrizalsy0424@gmail.com>
2021-02-27 17:18:46 +0700

    add multer-imgur

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,67 +1,89 @@
 const { Car } = require('../models')
+const imgur = require("imgur");
+const fs = require('fs')
 
 class CarController {
-    static findAll(req, res, next) {
-        Car.find()
-        .then(result => {
-            res.send(result)
-        })
-        .catch(error => {
+    static async findAll(req, res, next) {
+        try {
+            const cars = await Car.find()
+            if (cars) {
+                res.send(cars)            
+            } else {
+                throw(error);            
+            }            
+        } catch (error) {
             console.log(error);
-        })
+        }
     }
     
-    static findOne(req, res, next) {
-        const { id } = req.params
-        Car.findOne(id)
-        .then(result => {
-            res.send(result)
-        })
-        .catch(error => {
+    static async findOne(req, res, next) {
+        try {
+            const { id } = req.params
+            const car = await Car.findOne(id)
+            if (car) {
+                res.send(result)            
+            } else {
+                throw(error);            
+            }            
+        } catch (error) {
             console.log(error);
-        })
+        }
     }
 
-    static create(req, res, next) {
-        const { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price } = req.body
-        const payload = { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price }
+    static async create(req, res, next) {
+        try {
+            const file = req.files[0]
+            const url = await imgur.uploadFile(`../data/uploads/${file.filename}`)
+            const imageUrl = url.data.link
 
-        Car.create(payload)
-        .then(result => {
-            res.status(201)
-            res.send(result.ops[0])
-        })
-        .catch(error => {
+            const { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price } = req.body
+            const payload = { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price, imageUrl }
+    
+            const result = Car.create(payload)
+            if (result) {
+                res.status(201)
+                res.send(result.ops[0])
+                fs.unlinkSync(`../data/uploads/${file.filename}`)
+            } else {
+                throw(error)
+            }
+        } catch (error) {
             console.log(error);
-        })
+        }
     }
 
-    static update(req, res, next) {
-        const { id } = req.params
-        const { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price } = req.body
-        const payload = { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price }
+    static async update(req, res, next) {
+        try {
+            const { id } = req.params
+            const file = req.files[0]
+            const url = await imgur.uploadFile(`../data/uploads/${file.filename}`)
+            const imageUrl = url.data.link
 
-        Car.update(id, payload)
-        .then(result => {
-            res.status(200)
-            res.send(result.value)
-        })
-        .catch(error => {
+            const { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price } = req.body
+            const payload = { name, battery, acceleration, topSpeed, range, efficiency, fastCharge, price, imageUrl }
+    
+            const result = Car.update(id, payload)
+            if (result) {
+                res.status(200)
+                res.send(result.value)
+                fs.unlinkSync(`../data/uploads/${file.filename}`)
+            } else {
+                throw(error)
+            }
+        } catch (error) {
             console.log(error);
-        })
+        }
     }
 
-    static delete(req, res, next) {
-        const { id } = req.params
-        Car.delete(id)
-        .then(result => {
+    static async delete(req, res, next) {
+        try {
+            const { id } = req.params
+            await Car.delete(id)            
             res.send({message:'Succes to delete'})
-        })
-        .catch(error => {
+        } catch (error) {
             console.log(error);
-        })
+        }
     }
-
 }
 
 module.exports = CarController
